71beef5ce5e8dbbf439cb19e29971c72a1467f56
rcandelo <rcandelo@harlemnext.com>
2021-05-18 20:06:16 +0200

    wip: clock component (2)

--- ui/src/components/Clock.vue
+++ ui/src/components/Clock.vue
@@ -13,10 +13,26 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+
 @Component
 export default class Clock extends Vue {
+  private addMinutes = 0;
   get time(): string {
-    return process.env.VUE_APP_START_TIME;
+    return dayjs()
+      .hour(process.env.VUE_APP_START_TIME)
+      .startOf("hour")
+      .add(this.addMinutes, "minutes")
+      .format("LT");
+  }
+  created(): void {
+    dayjs.extend(localizedFormat);
+  }
+  mounted(): void {
+    setInterval((): void => {
+      this.addMinutes = this.addMinutes + 1;
+    }, 10000);
   }
 }
 </script>

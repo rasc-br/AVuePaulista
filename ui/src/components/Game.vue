02e6f121486e1433d4dea1c77972097b60025813
rcandelo <rcandelo@harlemnext.com>
2021-05-19 16:32:34 +0200

    wip: Game strucure + Moviments

--- ui/src/components/Game.vue
+++ ui/src/components/Game.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="bg-red-2" key="game-page">
-    <div class="row map-and-clock">
-      <div class="col-8 flex flex-center">map-position</div>
+    <div class="row movements-logo-clock">
+      <div class="col-6 flex"><Movements /></div>
+      <div class="col-2 flex">
+        <div class="col-12 flex flex-center q-pa-md avlogo">Add Logo Here</div>
+      </div>
       <div class="col-4 flex flex-center">
-        <div class="row logo-and-clock">
-          <div class="col-12 flex flex-center q-pa-md">Add Logo Here</div>
-          <div class="col-12 flex flex-center"><Clock /></div>
-        </div>
+        <div><Clock /></div>
       </div>
     </div>
     <div class="row actions-and-room">
@@ -22,12 +22,19 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Clock from "@/components/Clock.vue";
+import Movements from "@/components/Movements.vue";
 
 @Component({
   name: "Game",
   components: {
     Clock,
+    Movements,
   },
 })
 export default class Game extends Vue {}
 </script>
+<style lang="scss" scoped>
+.avlogo {
+  border: 1px solid black;
+}
+</style>
---
+++ ui/src/components/Movements.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="q-pa-md moviments">
+    <q-input
+      readonly
+      rounded
+      standout
+      class="current-position"
+      label="Current Position:"
+    >
+      <template v-slot:prepend>
+        <q-icon name="place" />
+      </template>
+    </q-input>
+    <div class="options flex justify-around">
+      <div v-for="place in placeOptions" :key="place">
+        <q-btn rounded class="to-go-place">
+          <q-icon class="explore" left size="2em" name="explore" />
+          <div>{{ place }}</div>
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Movements",
+})
+export default class Movements extends Vue {
+  get placeOptions(): string[] {
+    return ["Rua Augusta", "Alta Paulista", "Baixa Paulista", "Algum outro lugar"];
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .q-field--standout.q-field--readonly .q-field__control::before {
+  border: 1px solid rgba(0, 0, 0, 0.24);
+}
+.q-btn {
+  text-transform: none;
+}
+.moviments {
+  width: 100%;
+}
+.current-position {
+  max-width: 600px;
+  padding-bottom: 20px;
+}
+.to-go-place {
+  background: rgba(0, 0, 0, 0.05);
+  margin-bottom: 20px;
+  // min-width: 250px;
+}
+.explore {
+  color: rgba(0, 0, 0, 0.5);
+}
+.options {
+  max-width: 600px;
+}
+</style>

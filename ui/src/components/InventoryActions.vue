4f64a7af34c782c794cd3a7c629111728ee6af51
rcandelo <rcandelo@harlemnext.com>
2021-05-20 09:41:17 +0200

    feat: Add action buttons tooltip

--- ui/src/components/InventoryActions.vue
+++ ui/src/components/InventoryActions.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="q-pa-md room-actions">
-    <q-btn
-      class="icon-actions"
-      round
-      push
-      color="deep-purple-9"
-      text-color="white"
-      icon="build"
-    />
-    <q-btn
-      class="icon-actions"
-      round
-      push
-      color="green-10"
-      text-color="white"
-      icon="play_for_work"
-    />
+  <div class="q-pa-md inventory-actions">
+    <div class="row icon-row flex flex-center top-icon">
+      <q-btn
+        class="icon-actions"
+        round
+        push
+        color="deep-purple-9"
+        text-color="white"
+        icon="build"
+      >
+        <q-tooltip
+          transition-show="flip-right"
+          transition-hide="flip-left"
+          content-class="bg-deep-purple-9"
+          content-style="font-size: 16px"
+          anchor="top middle"
+          self="bottom middle"
+          :offset="[10, 10]"
+          :delay="1000"
+        >
+          Use
+        </q-tooltip>
+      </q-btn>
+    </div>
+    <div class="row icon-row flex flex-center bottom-icon">
+      <q-btn
+        class="icon-actions"
+        round
+        push
+        color="green-10"
+        text-color="white"
+        icon="play_for_work"
+      >
+        <q-tooltip
+          transition-show="flip-right"
+          transition-hide="flip-left"
+          content-class="bg-green-10"
+          content-style="font-size: 16px"
+          anchor="bottom middle"
+          self="top middle"
+          :offset="[10, 10]"
+          :delay="1000"
+        >
+          Drop
+        </q-tooltip>
+      </q-btn>
+    </div>
   </div>
 </template>
 
@@ -28,7 +58,14 @@ import { Component, Vue } from "vue-property-decorator";
 export default class InventoryActions extends Vue {}
 </script>
 <style lang="scss" scoped>
+.inventory-actions {
+  width: 100%;
+}
 .icon-actions {
   font-size: 1.5em;
+}
+.icon-row {
+  margin: 10px;
+  padding: 35px 0 35px 0;
 }
 </style>
--- ui/src/components/RoomActions.vue
+++ ui/src/components/RoomActions.vue
@@ -1,29 +1,74 @@
 <template>
   <div class="q-pa-md room-actions">
-    <q-btn
-      class="icon-actions"
-      round
-      push
-      color="red-10"
-      text-color="white"
-      icon="sports_mma"
-    />
-    <q-btn
-      class="icon-actions"
-      round
-      push
-      color="blue-9"
-      text-color="white"
-      icon="announcement"
-    />
-    <q-btn
-      class="icon-actions"
-      round
-      push
-      color="green-9"
-      text-color="white"
-      icon="back_hand"
-    />
+    <div class="row icon-row flex flex-center top-icon">
+      <q-btn
+        class="icon-actions"
+        round
+        push
+        color="red-10"
+        text-color="white"
+        icon="sports_mma"
+      >
+        <q-tooltip
+          transition-show="flip-right"
+          transition-hide="flip-left"
+          content-class="bg-red-10"
+          content-style="font-size: 16px"
+          anchor="top middle"
+          self="bottom middle"
+          :offset="[10, 10]"
+          :delay="1000"
+        >
+          Attack
+        </q-tooltip>
+      </q-btn>
+    </div>
+    <div class="row icon-row flex flex-center middle-icon">
+      <q-btn
+        class="icon-actions"
+        round
+        push
+        color="blue-9"
+        text-color="white"
+        icon="announcement"
+      >
+        <q-tooltip
+          transition-show="flip-right"
+          transition-hide="flip-left"
+          content-class="bg-blue-9"
+          content-style="font-size: 16px"
+          anchor="center left"
+          self="center right"
+          :offset="[10, 10]"
+          :delay="1000"
+        >
+          Shout
+        </q-tooltip>
+      </q-btn>
+    </div>
+    <div class="row icon-row flex flex-center bottom-icon">
+      <q-btn
+        class="icon-actions"
+        round
+        push
+        color="green-9"
+        text-color="white"
+        icon="back_hand"
+      >
+        <q-tooltip
+          transition-show="flip-right"
+          transition-hide="flip-left"
+          content-class="bg-green-9"
+          content-style="font-size: 16px"
+          anchor="bottom middle"
+          self="top middle"
+          :offset="[10, 10]"
+          :delay="1000"
+        >
+          Take
+        </q-tooltip>
+      </q-btn>
+    </div>
   </div>
 </template>
 
@@ -36,7 +81,20 @@ import { Component, Vue } from "vue-property-decorator";
 export default class RoomActions extends Vue {}
 </script>
 <style lang="scss" scoped>
+.room-actions {
+  width: 100%;
+}
 .icon-actions {
   font-size: 1.5em;
+}
+.icon-row {
+  margin: 10px;
+}
+.top-icon,
+.bottom-icon {
+  padding-right: 50px;
+}
+.middle-icon {
+  padding-left: 50px;
 }
 </style>

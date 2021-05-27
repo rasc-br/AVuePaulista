75f353513b48b6534f8a5a7ee7e9b5ba2d09388e
rcandelo <rcandelo@harlemnext.com>
2021-05-27 16:09:28 +0200

    feat: Icons combined with Room random items+characters

--- ui/src/components/Room.vue
+++ ui/src/components/Room.vue
@@ -1,10 +1,20 @@
 <template>
   <q-card class="room bg-red-3">
     <q-list>
-      <div v-for="object in gameObjects" :key="object">
+      <div
+        v-for="object in gameObjects"
+        :key="`${object.name}${object.health}`"
+      >
         <q-item clickable>
           <q-item-section avatar>
-            <q-icon :class="['side-icon', 'bombarder']" />
+            <q-icon
+              :class="[
+                'side-icon',
+                object.health
+                  ? `icon-char-${object.name}`
+                  : `icon-item-${object.name}`,
+              ]"
+            />
           </q-item-section>
 
           <q-item-section v-if="object.health">
@@ -54,8 +64,66 @@ export default class Room extends Vue {
 }
 .side-icon {
   padding-left: 7px;
-  &.bombarder {
-    content: url("../assets/bombarder.svg");
+  font-size: 80px !important;
+  &.icon-char-0 {
+    content: url("../assets/witch2.svg");
+  }
+  &.icon-char-1 {
+    content: url("../assets/brain2.svg");
+  }
+  &.icon-char-2 {
+    content: url("../assets/sorcerer.svg");
+  }
+  &.icon-char-3 {
+    content: url("../assets/bomberman2.svg");
+  }
+  &.icon-char-4 {
+    content: url("../assets/eagle2.svg");
+  }
+  &.icon-char-5 {
+    content: url("../assets/owl2.svg");
+  }
+  &.icon-char-6 {
+    content: url("../assets/demon.svg");
+  }
+  &.icon-item-0 {
+    content: url("../assets/book2.svg");
+  }
+  &.icon-item-1 {
+    content: url("../assets/sword.svg");
+  }
+  &.icon-item-2 {
+    content: url("../assets/hipnodisc2.svg");
+  }
+  &.icon-item-3 {
+    content: url("../assets/shield.svg");
+  }
+  &.icon-item-4 {
+    content: url("../assets/wax2.svg");
+  }
+  &.icon-item-5 {
+    content: url("../assets/hang-glider2.svg");
+  }
+  &.icon-item-6 {
+    content: url("../assets/spyglass2.svg");
+  }
+  &.icon-item-7 {
+    content: url("../assets/kitbomb2.svg");
+  }
+  &.icon-item-8 {
+    content: url("../assets/first-aid-kit.svg");
+  }
+  &.icon-item-9 {
+    content: url("../assets/gasmask2.svg");
+  }
+  &.icon-item-10 {
+    content: url("../assets/seta.svg");
+  }
+  &.icon-item-11 {
+    content: url("../assets/bomb.svg");
+  }
+  &.icon-item-12 {
+    content: url("../assets/gasbomb.svg");
   }
 }
 </style>

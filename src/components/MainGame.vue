58c340d99411dd8dea98ce3303713bd82e8d18d4
Raphael <raphael.candello@campbrain.com>
2024-01-14 22:07:53 -0500

    feat: Preparing Main Game

--- src/components/MainGame.vue
+++ src/components/MainGame.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import IntroAnimation from "./IntroAnimation.vue";
+import TheGame from "./TheGame.vue";
+import { storeToRefs } from "pinia";
+import { useAppStatus } from "../store/useAppStatus";
+import { IntroMode, GameMode } from "../../types";
+
+const appStatusStore = useAppStatus();
+const { introMode, gameMode } = storeToRefs(appStatusStore);
 </script>
 
 <template>
-  <div class="main-game">
-    <IntroAnimation />
+  <div class="avue-paulista">
+    <TheGame v-if="introMode === IntroMode.done" />
+    <IntroAnimation v-if="gameMode === GameMode.intro" />
   </div>
 </template>
 
